57200bc42430618e2cb3f657c9cf430adfe51cda
focusotter <mtliendo@focusotter.com>
2023-05-24 09:02:07 -0500

    Initial commit

---
+++ test/read-for-me-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReadForMeBackend from '../lib/read-for-me-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/read-for-me-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReadForMeBackend.ReadForMeBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
